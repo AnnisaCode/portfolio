07c09083f1da432e9416cce153055db2bf6dce42
Annisa <annisaseen@gmail.com>
2025-01-26 00:50:45 +0700

    up portfolio 2025

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            $('a.nav-link').removeClass('active');
+
+            $(this).addClass('active');
+
+            var hash = this.hash;
+            
+            $('html, body').stop().animate({
+                scrollTop: $(hash).offset().top - $('.navbar').outerHeight()
+            }, 800);
+        }
+    });
+});
+
